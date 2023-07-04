d64d90a5e3e48afa3433ce64f790c7a78e856263
Siddharth <120744527+Siddh0rth@users.noreply.github.com>
2023-07-04 21:46:04 +0530

    Detail update

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -57,8 +57,7 @@ const Landing = ({ setSelectedPage }) => {
           </p>
 
           <p className="mt-10 mb-7 text-sm text-center md:text-start">
-            Adipiscing arcu, in aliquam fringilla cursus. Elit arcu elementum
-            viverra malesuada sem ac faucibus dolor. Sagittis scelerisque.
+            My name is Siddharth Yadav, and I am a React.js developer. I am currently pursuing my B.Tech in Parul University and have been passionate about coding for several years. I take pride in my ability to develop user-friendly and intuitive web applications, paying attention to even the smallest details. As a coding enthusiast, I am always eager to learn and take on new challenges, striving to stay up-to-date with the latest web development trends and technologies. I believe that my strong work ethic and problem-solving skills make me a valuable asset to any team.
           </p>
         </motion.div>
 
